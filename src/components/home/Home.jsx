d975f6dfec046abd645ab2122492bb1dcd5dbc36
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-10-07 14:42:45 +0200

    bozza hompege

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,43 +2,43 @@ import { Col, Container, Row } from "react-bootstrap";
 
 function Home() {
   return (
-    <>
+    <div className="bg-dark">
       <Container>
         <Row className="">
           <Col>
             {" "}
-            <div className="drop">
+            <div className="drop text-center">
               <img
                 className="rounded-circle w-50 h-50"
                 src="https://www.throwup.it/wp-content-throwup/uploads/2021/04/micro-00-graffiti-artist-throwup-magazine.jpg"
                 alt="graff"
               />
-              <h4 className="ms-5">GRAFFITI</h4>
+              <h4>GRAFFITI</h4>
             </div>
           </Col>
           <Col>
-            <div className="drop">
+            <div className="drop text-center">
               <img
                 className="rounded-circle w-50 h-50"
                 src="https://www.throwup.it/wp-content-throwup/uploads/2021/04/micro-00-graffiti-artist-throwup-magazine.jpg"
                 alt="graff"
               />
-              <h4 className="ms-5">STREET-ART</h4>
+              <h4>STREET-ART</h4>
             </div>
           </Col>
           <Col>
-            <div className="drop">
+            <div className="drop text-center">
               <img
                 className="rounded-circle h-50 w-50"
-                src="https://www.throwup.it/wp-content-throwup/uploads/2021/04/micro-00-graffiti-artist-throwup-magazine.jpg"
+                src="https://storage.googleapis.com/pod_public/1300/167870.jpg"
                 alt="graff"
               />
-              <h4 className="ms-5">TAGS</h4>
+              <h4>TAGS</h4>
             </div>
           </Col>
         </Row>
       </Container>
-    </>
+    </div>
   );
 }
 
