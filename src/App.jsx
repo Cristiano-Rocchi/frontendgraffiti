1fb98f4c53601dea45a217f3940a7db44b851e83
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-10-14 14:28:13 +0200

    create pag profilo vuota

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Graffiti from "./components/graffiti/Graffiti";
 import Register from "./components/Register/Register";
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/graffiti" element={<Graffiti />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </>
     </BrowserRouter>
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,4 @@
+function Profile() {
+  return <h1>PAGINA PROFILO IN COSTRUZIONE</h1>;
+}
+export default Profile;
